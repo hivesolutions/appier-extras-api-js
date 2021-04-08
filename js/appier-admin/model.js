3225abc4de3df4d02453c4ca8a6884cd379b0e78
João Leal <jl@platforme.com>
2021-04-08 09:28:35 +0100

    feat: add listModels api call

--- js/appier-admin/model.js
+++ js/appier-admin/model.js
@@ -1,5 +1,10 @@
 export const ModelAPI = superclass =>
     class extends superclass {
+        async listModels(model, params = {}) {
+            const url = `${this.baseUrl}api/admin/models/${model}`;
+            const response = await this.get(url, { params: params });
+            return response;
+        }
         async createModel(model, payload) {
             const url = `${this.baseUrl}admin/models/${model}`;
             const response = await this.post(url, { dataJ: payload });
